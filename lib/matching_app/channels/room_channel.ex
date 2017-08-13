6872a8f1c4cdaf52da51112e56cec81fd4138db7
yuichitomita <y.tomita@speedlink-td.info>
2017-08-13 22:20:21 +0900

    fix

--- lib/matching_app/channels/room_channel.ex
+++ lib/matching_app/channels/room_channel.ex
@@ -2,23 +2,13 @@ defmodule MatchingApp.RoomChannel do
   use Phoenix.Channel
 
   def join("room:lobby", message, socket) do
-    Process.flag(:trap_exit, true)
-    :timer.send_interval(5000, :ping)
-    send(self, {:after_join, message})
-
     {:ok, socket}
   end
 
   def join("room:" <> _private_subtopic, _message, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
-
-  def handle_info({:after_join, msg}, socket) do
-      broadcast! socket, "user:entered", %{user: msg["user"]}
-      push socket, "join", %{status: "connected"}
-      {:noreply, socket}
-  end
-
+  
   def handle_in("new:msg", msg, socket) do
     broadcast! socket, "new:msg", %{user: msg["user"], body: msg["body"]}
     {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])}
