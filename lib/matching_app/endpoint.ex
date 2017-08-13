05c9416a360a8d5491ac6e4475259d19a20296b5
yuichitomita <y.tomita@speedlink-td.info>
2017-08-13 16:37:24 +0900

    chanel implemnet

--- lib/matching_app/endpoint.ex
+++ lib/matching_app/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule MatchingApp.Endpoint do
   use Phoenix.Endpoint, otp_app: :matching_app
 
-  socket "/room:*", MatchingApp.UserSocket
+  socket "/socket", MatchingApp.UserSocket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule MatchingApp.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", MatchingApp.RoomChannel
+  channel "room:*", MatchingApp.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
