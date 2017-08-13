8ce65ccf7b58877ef602389225404791a856824b
yuichitomita <y.tomita@speedlink-td.info>
2017-08-13 21:54:17 +0900

    fix room_channel.ex

--- lib/matching_app/channels/room_channel.ex
+++ lib/matching_app/channels/room_channel.ex
@@ -8,4 +8,14 @@ defmodule MatchingApp.RoomChannel do
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{user: msg["user"], body: msg["body"]}
+    {:reply, {:ok, %{msg: msg["body"]}}, assign(socket, :user, msg["user"])
+  end
+
+  def handle_out("new_msg", payload, socket) do
+    push socket, "new_msg", payload
+    {:noreply, socket}
+  end
 end
